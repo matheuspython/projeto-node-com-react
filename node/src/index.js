41224f41e775f566f1d3c51a255e4a374858545b
matheus souza <ruivorobado@gmail.com>
2021-07-12 20:36:49 -0300

    backend completo

---
+++ node/src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const {uuid} = require('uuidv4');
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+/* routes */
+const animes = []
+
+app.get('/animes',(req,res) => res.json(animes))
+
+app.post('/',(req, res) => {
+  const {anime,nota} = req.body;
+  const atualAnime = { id: uuid(), anime, nota };
+ 
+  animes.push(atualAnime);
+
+  return res.json(atualAnime);
+})
+
+app.delete('/:id', (req, res) => {
+  const { id } = req.params; 
+  
+  const findIndex = animes.findIndex(anime => anime.id === id);
+  animes.splice(findIndex,1);
+  if(findIndex>0) return res.json(animes)
+  
+  res.status(400).json({erro: 'ocoreu um erro'})
+})
+
+
+
+app.listen(8888, ()=> console.log('server rodando'))
